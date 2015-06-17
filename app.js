23f9fa5f0e3cd1cf759bd050ea93302803ec340b
Anita <a.nita@hotmail.co.uk>
2015-06-17 13:41:46 +0100

    deleted files

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-// var MongoClient = require('mongodb').MongoClient,
-//    	assert = require('assert');
+var MongoClient = require('mongodb').MongoClient,
+   	assert = require('assert');
 
-// // Connection URL
-// var url = 'mongodb://localhost:27017/overshare';
-// // Use connect method to connect to the Server
-// MongoClient.connect(url, function(err, db) {
-// 	assert.equal(null, err);
-// 	console.log("Connected correctly to server");
+// Connection URL
+var url = 'mongodb://localhost:27017/overshare';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+	assert.equal(null, err);
+	console.log("Connected correctly to server");
 
-// 	insertDocuments(db, function() {
-// 	    db.close();
-// 	});
-// });
+	insertDocuments(db, function() {
+	    db.close();
+	});
+});
 
-// var insertDocuments = function(db, callback) {
-//   	// Get the documents collection
-//   	var collection = db.collection('documents');
-//   	// Insert some documents
-//   	collection.insert([{a : 1}, {a : 2}, {a : 3}], function(err, result) {
-// 	    // assert.equal(err, null);
-// 	    // assert.equal(3, result.result.n);
-// 	    // assert.equal(3, result.ops.length);
-// 	    console.log("Inserted 3 documents into the document collection");
-// 	    callback(result);
-//   });
-// };
+var insertDocuments = function(db, callback) {
+  	// Get the documents collection
+  	var collection = db.collection('documents');
+  	// Insert some documents
+  	collection.insert([{a : 1}, {a : 2}, {a : 3}], function(err, result) {
+	    // assert.equal(err, null);
+	    // assert.equal(3, result.result.n);
+	    // assert.equal(3, result.ops.length);
+	    console.log("Inserted 3 documents into the document collection");
+	    callback(result);
+  });
+};
