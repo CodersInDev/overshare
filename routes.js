9c0e78f298dd44fb47116bf9c45d8c34b1caaa27
rub1e <alex.rubner@gmail.com>
2015-06-17 13:19:05 +0100

    created html

--- routes.js
+++ routes.js
@@ -1,11 +1,67 @@
+var databaseConfig = require('./config.js'),
+    db = require('level')(databaseConfig.database),
+    dbHelper = require('./databaseHelpers.js'),
+    database = new dbHelper(db),
+    Bcrypt = require('bcrypt');
+
 var routes = [
 	{
 		path: "/",
 		method: "GET",
 		handler: function(request, reply) {
-			reply("Testing response");
+			reply.file("index.html");
 		}
-	}
+	},
+	{
+	    method: 'GET',
+	    path: '/static/{path*}',
+	    handler:  {
+	      directory: {
+	        path: './'
+	      }
+	    }
+	},
+	{
+		path: '/auth',
+		method: 'GET',
+		handler: function(request, reply){
+			reply.file("./register.html");
+		}
+	},
+
+	{
+		path: '/auth',
+		method: 'POST',
+		handler: function(request, reply){
+      Bcrypt.genSalt(10, function(err, salt) {
+        Bcrypt.hash(request.payload.password, salt, function(err, hash) {
+          database.addUser(request.payload.email, hash, function(result){
+            if(!result){
+              reply("Can't add the user");
+            }else{
+              reply(result);
+            }
+          });
+        });
+      });
+    }
+  },
+
+  {
+    path: '/login',
+    method: 'GET',
+    handler: function(request, reply){
+      reply("page login");
+    }
+  },
+
+  {
+    path: '/login',
+    method: 'POST',
+    handler: function(request, reply){
+      reply("post on login");
+    }
+  }
 ];
 
 module.exports = routes;
